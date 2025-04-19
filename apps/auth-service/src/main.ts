c2738b7c036a1061e5b0ffe66aa362edb974dab7
AnonymousFreelancer17 <AnonymousFreelancer17@gmail.com>
2025-04-19 15:52:41 +0530

    Setting Db and Auth Services

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,14 +1,33 @@
-import express from 'express';
+import express from "express";
+import cors from "cors";
+import { errorMiddleware } from '../../../packages/error_handler/error_middleware';
+import cookieParser from "cookie-parser";
 
-const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 const app = express();
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    allowedHeaders: ["Authorization", "Content-Type"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    res.send({ 'message': 'Hello API'});
+const port = process.env.PORT || 6001;
+const server = app.listen(port, () => {
+  console.log(`[ Auth Services ready ] http://localhost:${port}/api`);
 });
 
-app.listen(port, host, () => {
-    console.log(`[ ready ] http://${host}:${port}`);
+app.get("/", (req, res) => {
+  res.send({
+    message: "Hello from Auth Services",
+  });
+});
+
+app.use(errorMiddleware);
+
+server.on("error", (err) => {
+  console.log("Server Error : ", err);
 });
